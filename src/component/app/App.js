8fe5e1482eb26621d64643e1c55488f3daa6ae48
abbassDaher <abbassDaher1@outlook.com>
2024-02-26 09:54:15 +0200

    create reducer and listen for Acc component

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState,} from "react";
+import { useSelector } from "react-redux";
+import "./App.css";
 import ButtonComponent from "../button/ButtonComponent";
 
 function App() {
   const [counter, setCounter] = useState(0);
-
+  const state = useSelector((state)=> state.value )
+console.log(state);
   function funCounter(operator) {
     if (operator == "increase")
       return setCounter(counter + 1);
@@ -14,9 +17,11 @@ function App() {
 
     <div className="App">
       <h1>hello redux basic</h1>
-      <div className="">counter:<span className="counter">{counter}</span></div>
-      <ButtonComponent funCounter={funCounter} >increase</ButtonComponent>
-      <ButtonComponent funCounter={funCounter} >decrease</ButtonComponent>
+      <div className="">counter:<span className="counter">{state}</span></div>
+      <div className="container">
+        <ButtonComponent funCounter={funCounter} >increase</ButtonComponent>
+        <ButtonComponent funCounter={funCounter} >decrease</ButtonComponent>
+      </div>
 
 
     </div>
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux"
+
+const initState = { value: 0 }
+const counterReducer = (state = initState, action) => {
+    return state
+}
+const store = createStore(counterReducer)
+
+export default store
