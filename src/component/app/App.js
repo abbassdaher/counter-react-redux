64378c7b171f34524a495ce74e2b72b85cdbe4cf
abbassDaher <abbassDaher1@outlook.com>
2024-02-27 16:30:48 +0200

    add action and dispatch

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -1,17 +1,22 @@
-import { useState,} from "react";
-import { useSelector } from "react-redux";
+import { useState, } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 import ButtonComponent from "../button/ButtonComponent";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const state = useSelector((state)=> state.value )
-console.log(state);
-  function funCounter(operator) {
-    if (operator == "increase")
-      return setCounter(counter + 1);
-    else return setCounter(counter - 1);
-  };
+  const state = useSelector((state) => state.value)
+  const dispatch = useDispatch()
+  
+  const increase = () => {
+    const action = { type: 'increase' }
+    dispatch(action)
+  }
+  const decrease = () => {
+  const action = {type : 'decrease' }
+  dispatch(action)
+  }
+
 
   return (
 
@@ -19,8 +24,10 @@ console.log(state);
       <h1>hello redux basic</h1>
       <div className="">counter:<span className="counter">{state}</span></div>
       <div className="container">
-        <ButtonComponent funCounter={funCounter} >increase</ButtonComponent>
-        <ButtonComponent funCounter={funCounter} >decrease</ButtonComponent>
+        
+        <button onClick={increase} >increase +</button>
+        <button onClick={decrease}>decrease -</button>
+
       </div>
 
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,11 @@ import { createStore } from "redux"
 
 const initState = { value: 0 }
 const counterReducer = (state = initState, action) => {
+    console.log(action.type);
+    if (action.type == 'increase') { return { ...state, value: state.value += 1 } } else if (action.type == 'decrease') {
+        return { ...state, value: state.value -= 1 }
+    }
     return state
 }
 const store = createStore(counterReducer)
-
 export default store
