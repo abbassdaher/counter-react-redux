e3598c85661885cb78949fb95be5e0a7fcdad8d1
abbassDaher <abbassDaher1@outlook.com>
2024-02-28 16:06:10 +0200

    add toggleCounter

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -1,4 +1,4 @@
-import { useState, } from "react";
+import { Fragment, useState, } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 import ButtonComponent from "../button/ButtonComponent";
@@ -7,26 +7,35 @@ function App() {
   const [counter, setCounter] = useState(0);
   const state = useSelector((state) => state.value)
   const dispatch = useDispatch()
-  
+
   const increase = () => {
     const action = { type: 'increase' }
     dispatch(action)
   }
   const decrease = () => {
-  const action = {type : 'decrease' }
-  dispatch(action)
+    const action = { type: 'decrease' }
+    dispatch(action)
   }
-
+  const stateToggle = useSelector((state) => state.showCounter)
+ const toggleConter = ()=>{
+  dispatch({ type: 'toggleConter'})
+ }
 
   return (
 
     <div className="App">
       <h1>hello redux basic</h1>
-      <div className="">counter:<span className="counter">{state}</span></div>
+      {stateToggle && <Fragment>
+        <div className="">counter:
+        <span className="counter">{state}</span>
+      </div>
+      </Fragment>}
+
       <div className="container">
-        
+
         <button onClick={increase} >increase +</button>
         <button onClick={decrease}>decrease -</button>
+        <button onClick={toggleConter}>show content</button>
 
       </div>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
 import { createStore } from "redux"
 
-const initState = { value: 0 }
+const initState = { value: 0 ,  showCounter: true}
 const counterReducer = (state = initState, action) => {
     console.log(action.type);
-    if (action.type == 'increase') { return { ...state, value: state.value += 1 } } else if (action.type == 'decrease') {
+    if (action.type == 'increase') {
+         return { ...state, value: state.value += 1 }
+         } else if (action.type == 'decrease') {
         return { ...state, value: state.value -= 1 }
+    }
+    if(action.type == 'toggleConter'){
+        return {...state, showCounter:!state.showCounter }
     }
     return state
 }
